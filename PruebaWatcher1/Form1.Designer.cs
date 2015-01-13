06e199bbfdc056de800b3b75cd5a07c28663ac87
ArturooTz <jtz307@hotmail.com>
2015-01-13 13:30:51 -0700

    W

---
+++ PruebaWatcher1/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace PruebaWatcher1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(220, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(7, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Set Directory";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(448, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(526, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(117, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Current directory:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(118, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Downloads status:";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(7, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 68);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Start Watch!";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.Enabled = false;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(7, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Re-check folder";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(285, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(170, 37);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Watch is OFF";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(473, 1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(53, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "_";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.ForeColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(123, 154);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Turn Off";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.ForeColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(123, 178);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Sleep";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton3.ForeColor = System.Drawing.Color.White;
+            this.radioButton3.Location = new System.Drawing.Point(123, 201);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(71, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Hibernate";
+            this.radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(120, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "When downloads finish:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PruebaWatcher1.Properties.Resources.steambg13;
+            this.ClientSize = new System.Drawing.Size(580, 230);
+            this.ControlBox = false;
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Steam Downloads Watcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ PruebaWatcher1/Form1.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Management;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace PruebaWatcher1
+{
+    public partial class Form1 : Form
+    {
+        string path1 = "", path2 = "", path3 = "", path5 = "";
+        bool exist1 = false, fin1 = false;
+        System.Timers.Timer t = new System.Timers.Timer(TimeSpan.FromSeconds(2).TotalMilliseconds); // set the time
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            path2 = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string locat1 = path2 + "\\sdlocat.txt";
+            label4.ForeColor = Color.Red;
+            if (File.Exists(locat1))
+            {
+                using (StreamReader reader = new StreamReader(locat1))
+                {
+                    path3 = reader.ReadLine();
+                    //exist1 = true;
+                    AsignPathFromLocal();
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+
+        #region BOTONES
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AsignPathFromSearch(folderBrowserDialog1);
+            //if (exist1 == false)
+            //{
+
+            //}
+            //else
+            //{
+
+            //}
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            path5 = textBox1.Text;
+
+            label4.Text = "Watch is ON";
+            label4.ForeColor = Color.Green;
+
+            button3.Enabled = false;
+
+            t.AutoReset = true;
+
+            t.Elapsed += new System.Timers.ElapsedEventHandler(CheckFolder);
+            
+            t.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DirectoryCheckChanges();
+            //if (IsDirectoryEmpty(path3) == true)
+            //{
+            //    label1.Text = "There are no downloads in progress.";
+            //    label1.ForeColor = Color.Red;
+            //    button3.Enabled = false;
+            //}
+            //else
+            //{
+            //    label1.Text = "There is a download in progress!";
+            //    label1.ForeColor = Color.Green;
+            //    button3.Enabled = true;
+            //}
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        #endregion
+
+       
+        #region METODOS
+
+        public void AsignPathFromSearch(FolderBrowserDialog kek1)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string locat = "";
+                locat = path2 + "\\sdlocat.txt";
+                path1 = folderBrowserDialog1.SelectedPath;
+                System.IO.File.WriteAllText(@locat, path1);
+                button4.Enabled = true;
+                if (path1.Contains("Steam\\steamapps\\downloading"))
+                {
+                    textBox1.Text = path1;
+                    if (IsDirectoryEmpty(path1) == true)
+                    {
+                        label1.Text = "There are no downloads in progress.";
+                        label1.ForeColor = Color.Red;
+                        button3.Enabled = false;
+                    }
+                    else
+                    {
+                        label1.Text = "There is a download in progress!";
+                        label1.ForeColor = Color.Green;
+                        button3.Enabled = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("That is not the Steam Downloads folder.");
+                }
+
+            }
+
+        }
+
+        public void DirectoryCheckChanges()
+        {
+            if (IsDirectoryEmpty(path3) == true)
+            {
+                label1.Text = "There are no downloads in progress.";
+                label1.ForeColor = Color.Red;
+                button3.Enabled = false;
+            }
+            else
+            {
+                label1.Text = "There is a download in progress!";
+                label1.ForeColor = Color.Green;
+                button3.Enabled = true;
+            }
+        }
+
+        public void AsignPathFromLocal()
+        {
+            textBox1.Text = path3;
+            button4.Enabled = true;
+            DirectoryCheckChanges();
+            //if (IsDirectoryEmpty(path3) == true)
+            //{
+            //    label1.Text = "There are no downloads in progress.";
+            //    label1.ForeColor = Color.Red;
+            //    button3.Enabled = false;
+            //}
+            //else
+            //{
+            //    label1.Text = "There is a download in progress!";
+            //    label1.ForeColor = Color.Green;
+            //    button3.Enabled = true;
+            //}
+
+        }
+
+        public bool IsDirectoryEmpty(string path)
+        {
+            path = textBox1.Text;
+            return !Directory.EnumerateFileSystemEntries(path).Any();
+            //IEnumerable<string> items = Directory.EnumerateFileSystemEntries(path);
+            //using (IEnumerator<string> en = items.GetEnumerator())
+            //{
+            //    return !en.MoveNext();
+            //}
+        }
+
+        public void CheckFolder(object sender, ElapsedEventArgs e)
+        {
+            //MessageBox.Show("Top Kek m8");
+            
+            if (IsDirectoryEmpty(path5))
+            {
+                if (t.Enabled == true)
+                {
+                    t.AutoReset = false;
+                    t.Stop();
+                    t.Enabled = false;
+                    SetText("Downloads have finished, the system will now shutdown!");
+                    
+                }
+     
+               
+                System.Timers.Timer j = new System.Timers.Timer(TimeSpan.FromSeconds(10).TotalMilliseconds); // set the time
+
+                j.AutoReset = false;
+
+                j.Elapsed += new System.Timers.ElapsedEventHandler(TurnOff1);
+
+                j.Start();
+
+                
+            }
+        }
+
+        public void TurnOff1(object sender, ElapsedEventArgs e)
+        {
+            SetText4("Watch is OFF");
+
+            if (radioButton1.Checked == true)
+            {
+                List<string> lel = new List<string>();
+                Process[] localAll = Process.GetProcesses();
+                for (sbyte i = 0; localAll.Count() > i; i++)
+                {
+                    if (localAll[i].ProcessName == "Steam")
+                    {
+                        localAll[i].Kill();
+                    }
+                }
+                Process.Start("shutdown", "/s /t 0");
+                Application.Exit();
+                
+            }
+            else if (radioButton2.Checked == true)
+            {
+                SetSuspendState(false, true, true);
+            }
+            else if (radioButton3.Checked == true)
+            {
+                SetSuspendState(true, true, true);
+
+            }
+            //MessageBox.Show("SYSTEM OFF");
+            
+            
+            //Application.Exit();
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label1.Text = text;
+            }
+        }
+
+        private void SetText4(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText4);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label4.Text = text;
+                this.label4.ForeColor = Color.Red;
+            }
+        }
+
+        [DllImport("PowrProf.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+
+
+        /////////////////// MOVER EL FORM SIN BORDE ///////////////////
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd,
+                         int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        ///////////////////////////////////////////////////////////////
+        
+
+
+        #endregion
+
+        
+
+        
+    }
+}
